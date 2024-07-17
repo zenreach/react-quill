b2ca3b244051cb401e4daa287ed47fc3ac74292d
eshang-adentro <eshang@adentro.com>
2024-07-17 16:18:57 -0400

    upgrade to react 16

--- src/component.js
+++ src/component.js
@@ -1,296 +1,339 @@
 'use strict';
 
+
 var React = require('react'),
-	ReactDOM = require('react-dom'),
-	QuillToolbar = require('./toolbar'),
-	QuillMixin = require('./mixin'),
-	T = React.PropTypes;
+   ReactDOM = require('react-dom'),
+   QuillToolbar = require('./toolbar'),
+   QuillMixin = require('./mixin'),
+   T = require('prop-types'),
+   createClass = require('create-react-class'),
+   DOM = require('react-dom-factories');
+
 
 // FIXME: Remove with the switch to JSX
 QuillToolbar = React.createFactory(QuillToolbar);
 
+
 var find = function(arr, predicate) {
-	if (!arr) {
-		return;
-	}
-	for (var i=0; i<arr.length; ++i) {
-		if (predicate(arr[i])) return arr[i];
-	}
+   if (!arr) {
+       return;
+   }
+   for (var i=0; i<arr.length; ++i) {
+       if (predicate(arr[i])) return arr[i];
+   }
 }
 
-var QuillComponent = React.createClass({
 
-	displayName: 'Quill',
+var QuillComponent = createClass({
 
-	mixins: [ QuillMixin ],
 
-	propTypes: {
-		id: T.string,
-		className: T.string,
-		style: T.object,
-		value: T.string,
-		defaultValue: T.string,
-		readOnly: T.bool,
-		modules: T.object,
-		toolbar: T.oneOfType([ T.array, T.oneOf([false]), ]),
-		formats: T.array,
-		styles: T.oneOfType([ T.object, T.oneOf([false]) ]),
-		theme: T.string,
-		pollInterval: T.number,
-		onKeyPress: T.func,
-		onKeyDown: T.func,
-		onKeyUp: T.func,
-		onChange: T.func,
-		onChangeSelection: T.func
-	},
+   displayName: 'Quill',
 
-	/*
-	Changing one of these props should cause a re-render.
-	*/
-	dirtyProps: [
-		'id',
-		'className',
-		'modules',
-		'toolbar',
-		'formats',
-		'styles',
-		'theme',
-		'pollInterval'
-	],
 
-	getDefaultProps: function() {
-		return {
-			className: '',
-			theme: 'snow',
-			modules: {}
-		};
-	},
+   mixins: [ QuillMixin ],
 
-	/*
-	We consider the component to be controlled if
-	whenever `value` is bein sent in props.
-	*/
-	isControlled: function() {
-		return 'value' in this.props;
-	},
 
-	getInitialState: function() {
-		return {
-			value: this.isControlled()
-				? this.props.value
-				: this.props.defaultValue
-		};
-	},
+   propTypes: {
+       id: T.string,
+       className: T.string,
+       style: T.object,
+       value: T.string,
+       defaultValue: T.string,
+       readOnly: T.bool,
+       modules: T.object,
+       toolbar: T.oneOfType([ T.array, T.oneOf([false]), ]),
+       formats: T.array,
+       styles: T.oneOfType([ T.object, T.oneOf([false]) ]),
+       theme: T.string,
+       pollInterval: T.number,
+       onKeyPress: T.func,
+       onKeyDown: T.func,
+       onKeyUp: T.func,
+       onChange: T.func,
+       onChangeSelection: T.func
+   },
 
-	componentWillReceiveProps: function(nextProps) {
-		var editor = this.state.editor;
-		// If the component is unmounted and mounted too quickly
-		// an error is thrown in setEditorContents since editor is
-		// still undefined. Must check if editor is undefined
-		// before performing this call.
-		if (editor) {
-			// Update only if we've been passed a new `value`.
-			// This leaves components using `defaultValue` alone.
-			if ('value' in nextProps) {
-				// NOTE: Seeing that Quill is missing a way to prevent
-				//       edits, we have to settle for a hybrid between
-				//       controlled and uncontrolled mode. We can't prevent
-				//       the change, but we'll still override content
-				//       whenever `value` differs from current state.
-				if (nextProps.value !== this.getEditorContents()) {
-					this.setEditorContents(editor, nextProps.value);
-				}
-			}
-			// We can update readOnly state in-place.
-			if ('readOnly' in nextProps) {
-				if (nextProps.readOnly !== this.props.readOnly) {
-					this.setEditorReadOnly(editor, nextProps.readOnly);
-				}
-			}
-		}
-	},
 
-	componentDidMount: function() {
-		var editor = this.createEditor(
-			this.getEditorElement(),
-			this.getEditorConfig());
+   /*
+   Changing one of these props should cause a re-render.
+   */
+   dirtyProps: [
+       'id',
+       'className',
+       'modules',
+       'toolbar',
+       'formats',
+       'styles',
+       'theme',
+       'pollInterval'
+   ],
 
-		this.setCustomFormats(editor);
-		var fontOptions = document.querySelectorAll('.quill-toolbar .ql-font.ql-picker .ql-picker-item');
-		for (var i=0; i<fontOptions.length; ++i) {
-			fontOptions[i].style.fontFamily = fontOptions[i].dataset.value;
-		}
 
-		this.setState({ editor:editor });
-	},
+   getDefaultProps: function() {
+       return {
+           className: '',
+           theme: 'snow',
+           modules: {}
+       };
+   },
 
-	componentWillUnmount: function() {
-		// NOTE: Don't set the state to null here
-		//       as it would generate a loop.
 
-		this.state.removeEventsCallback();
-	},
+   /*
+   We consider the component to be controlled if
+   whenever `value` is bein sent in props.
+   */
+   isControlled: function() {
+       return 'value' in this.props;
+   },
 
-	shouldComponentUpdate: function(nextProps, nextState) {
-		// Check if one of the changes should trigger a re-render.
-		for (var i=0; i<this.dirtyProps.length; i++) {
-			var prop = this.dirtyProps[i];
-			if (nextProps[prop] !== this.props[prop]) {
-				return true;
-			}
-		}
-		// Never re-render otherwise.
-		return false;
-	},
 
-	/*
-	If for whatever reason we are rendering again,
-	we should tear down the editor and bring it up
-	again.
-	*/
-	componentWillUpdate: function() {
-		this.componentWillUnmount();
-	},
+   getInitialState: function() {
+       return {
+           value: this.isControlled()
+               ? this.props.value
+               : this.props.defaultValue
+       };
+   },
 
-	componentDidUpdate: function() {
-		this.componentDidMount();
-	},
 
-	setCustomFormats: function (editor) {
-		if (!this.props.formats) {
-			return;
-		}
+   componentWillReceiveProps: function(nextProps) {
+       var editor = this.state.editor;
+       // If the component is unmounted and mounted too quickly
+       // an error is thrown in setEditorContents since editor is
+       // still undefined. Must check if editor is undefined
+       // before performing this call.
+       if (editor) {
+           // Update only if we've been passed a new `value`.
+           // This leaves components using `defaultValue` alone.
+           if ('value' in nextProps) {
+               // NOTE: Seeing that Quill is missing a way to prevent
+               //       edits, we have to settle for a hybrid between
+               //       controlled and uncontrolled mode. We can't prevent
+               //       the change, but we'll still override content
+               //       whenever `value` differs from current state.
+               if (nextProps.value !== this.getEditorContents()) {
+                   this.setEditorContents(editor, nextProps.value);
+               }
+           }
+           // We can update readOnly state in-place.
+           if ('readOnly' in nextProps) {
+               if (nextProps.readOnly !== this.props.readOnly) {
+                   this.setEditorReadOnly(editor, nextProps.readOnly);
+               }
+           }
+       }
+   },
 
-		for (var i = 0; i < this.props.formats.length; i++) {
-			var format = this.props.formats[i];
-			editor.addFormat(format.name || format, format);
-		}
-	},
 
-	getEditorConfig: function() {
-		var config = {
-			readOnly:     this.props.readOnly,
-			theme:        this.props.theme,
-			// Let Quill set the defaults, if no formats supplied
-			formats:      this.props.formats ? [] : undefined,
-			styles:       this.props.styles,
-			modules:      this.props.modules,
-			pollInterval: this.props.pollInterval
-		};
-		// Unless we're redefining the toolbar, or it has been explicitly
-		// disabled, attach to the default one as a ref.
-		if (this.props.toolbar !== false && !config.modules.toolbar) {
-			// Don't mutate the original modules
-			// because it's shared between components.
-			config.modules = JSON.parse(JSON.stringify(config.modules));
-			config.modules.toolbar = {
-				container: ReactDOM.findDOMNode(this.refs.toolbar)
-			}
-		}
-		return config;
-	},
+   componentDidMount: function() {
+       var editor = this.createEditor(
+           this.getEditorElement(),
+           this.getEditorConfig());
 
-	getEditor: function() {
-		return this.state.editor;
-	},
 
-	getEditorElement: function() {
-		return ReactDOM.findDOMNode(this.refs.editor);
-	},
+       this.setCustomFormats(editor);
+       var fontOptions = document.querySelectorAll('.quill-toolbar .ql-font.ql-picker .ql-picker-item');
+       for (var i=0; i<fontOptions.length; ++i) {
+           fontOptions[i].style.fontFamily = fontOptions[i].dataset.value;
+       }
 
-	getEditorContents: function() {
-		return this.state.value;
-	},
 
-	getEditorSelection: function() {
-		return this.state.selection;
-	},
+       this.setState({ editor:editor });
+   },
 
-	/*
-	Renders either the specified contents, or a default
-	configuration of toolbar and contents area.
-	*/
-	renderContents: function() {
-		var contents = [];
-		var children = React.Children.map(
-			this.props.children,
-			function(c) { return React.cloneElement(c, {ref: c.ref}); }
-		);
 
-		if (this.props.toolbar !== false) {
-			var toolbar = find(children, function(child) {
-				return child.ref === 'toolbar';
-			})
-			contents.push(toolbar ? toolbar : QuillToolbar({
-				key: 'toolbar-' + Math.random(),
-				ref: 'toolbar',
-				items: this.props.toolbar
-			}))
-		}
+   componentWillUnmount: function() {
+       // NOTE: Don't set the state to null here
+       //       as it would generate a loop.
 
-		var editor = find(children, function(child) {
-			return child.ref === 'editor';
-		})
-		contents.push(editor ? editor : React.DOM.div({
-			key: 'editor-' + Math.random(),
-			ref: 'editor',
-			className: 'quill-contents',
-			dangerouslySetInnerHTML: { __html:this.getEditorContents() }
-		}))
 
-		return contents;
-	},
+       this.state.removeEventsCallback();
+   },
 
-	render: function() {
-		return React.DOM.div({
-			id: this.props.id,
-			style: this.props.style,
-			className: ['quill'].concat(this.props.className).join(' '),
-			onKeyPress: this.props.onKeyPress,
-			onKeyDown: this.props.onKeyDown,
-			onKeyUp: this.props.onKeyUp,
-			onChange: this.preventDefault },
-			this.renderContents()
-		);
-	},
 
-	onEditorChange: function(value, delta, source, editor) {
-		if (value !== this.getEditorContents()) {
-			this.setState({ value: value });
-			if (this.props.onChange) {
-				this.props.onChange(value, delta, source, editor);
-			}
-		}
-	},
+   shouldComponentUpdate: function(nextProps, nextState) {
+       // Check if one of the changes should trigger a re-render.
+       for (var i=0; i<this.dirtyProps.length; i++) {
+           var prop = this.dirtyProps[i];
+           if (nextProps[prop] !== this.props[prop]) {
+               return true;
+           }
+       }
+       // Never re-render otherwise.
+       return false;
+   },
 
-	onEditorChangeSelection: function(range, source, editor) {
-		var s = this.getEditorSelection() || {};
-		var r = range || {};
-		if (r.length !== s.length || r.index !== s.index) {
-			this.setState({ selection: range });
-			if (this.props.onChangeSelection) {
-				this.props.onChangeSelection(range, source, editor);
-			}
-		}
-	},
 
-	focus: function() {
-		this.state.editor.focus();
-	},
+   /*
+   If for whatever reason we are rendering again,
+   we should tear down the editor and bring it up
+   again.
+   */
+   componentWillUpdate: function() {
+       this.componentWillUnmount();
+   },
 
-	blur: function() {
-		this.setEditorSelection(this.state.editor, null);
-	},
 
-	/*
-	Stop change events from the toolbar from
-	bubbling up outside.
-	*/
-	preventDefault: function(event) {
-		event.preventDefault();
-		event.stopPropagation();
-	}
+   componentDidUpdate: function() {
+       this.componentDidMount();
+   },
+
+
+   setCustomFormats: function (editor) {
+       if (!this.props.formats) {
+           return;
+       }
+
+
+       for (var i = 0; i < this.props.formats.length; i++) {
+           var format = this.props.formats[i];
+           editor.addFormat(format.name || format, format);
+       }
+   },
+
+
+   getEditorConfig: function() {
+       var config = {
+           readOnly:     this.props.readOnly,
+           theme:        this.props.theme,
+           // Let Quill set the defaults, if no formats supplied
+           formats:      this.props.formats ? [] : undefined,
+           styles:       this.props.styles,
+           modules:      this.props.modules,
+           pollInterval: this.props.pollInterval
+       };
+       // Unless we're redefining the toolbar, or it has been explicitly
+       // disabled, attach to the default one as a ref.
+       if (this.props.toolbar !== false && !config.modules.toolbar) {
+           // Don't mutate the original modules
+           // because it's shared between components.
+           config.modules = JSON.parse(JSON.stringify(config.modules));
+           config.modules.toolbar = {
+               container: ReactDOM.findDOMNode(this.refs.toolbar)
+           }
+       }
+       return config;
+   },
+
+
+   getEditor: function() {
+       return this.state.editor;
+   },
+
+
+   getEditorElement: function() {
+       return ReactDOM.findDOMNode(this.refs.editor);
+   },
+
+
+   getEditorContents: function() {
+       return this.state.value;
+   },
+
+
+   getEditorSelection: function() {
+       return this.state.selection;
+   },
+
+
+   /*
+   Renders either the specified contents, or a default
+   configuration of toolbar and contents area.
+   */
+   renderContents: function() {
+       var contents = [];
+       var children = React.Children.map(
+           this.props.children,
+           function(c) { return React.cloneElement(c, {ref: c.ref}); }
+       );
+
+
+       if (this.props.toolbar !== false) {
+           var toolbar = find(children, function(child) {
+               return child.ref === 'toolbar';
+           })
+           contents.push(toolbar ? toolbar : QuillToolbar({
+               key: 'toolbar-' + Math.random(),
+               ref: 'toolbar',
+               items: this.props.toolbar
+           }))
+       }
+
+
+       var editor = find(children, function(child) {
+           return child.ref === 'editor';
+       })
+       contents.push(editor ? editor : DOM.div({
+           key: 'editor-' + Math.random(),
+           ref: 'editor',
+           className: 'quill-contents',
+           dangerouslySetInnerHTML: { __html:this.getEditorContents() }
+       }))
+
+
+       return contents;
+   },
+
+
+   render: function() {
+       return DOM.div({
+           id: this.props.id,
+           style: this.props.style,
+           className: ['quill'].concat(this.props.className).join(' '),
+           onKeyPress: this.props.onKeyPress,
+           onKeyDown: this.props.onKeyDown,
+           onKeyUp: this.props.onKeyUp,
+           onChange: this.preventDefault },
+           this.renderContents()
+       );
+   },
+
+
+   onEditorChange: function(value, delta, source, editor) {
+       if (value !== this.getEditorContents()) {
+           this.setState({ value: value });
+           if (this.props.onChange) {
+               this.props.onChange(value, delta, source, editor);
+           }
+       }
+   },
+
+
+   onEditorChangeSelection: function(range, source, editor) {
+       var s = this.getEditorSelection() || {};
+       var r = range || {};
+       if (r.length !== s.length || r.index !== s.index) {
+           this.setState({ selection: range });
+           if (this.props.onChangeSelection) {
+               this.props.onChangeSelection(range, source, editor);
+           }
+       }
+   },
+
+
+   focus: function() {
+       this.state.editor.focus();
+   },
+
+
+   blur: function() {
+       this.setEditorSelection(this.state.editor, null);
+   },
+
+
+   /*
+   Stop change events from the toolbar from
+   bubbling up outside.
+   */
+   preventDefault: function(event) {
+       event.preventDefault();
+       event.stopPropagation();
+   }
+
 
 });
 
+
 module.exports = QuillComponent;
+
+
--- src/toolbar.js
+++ src/toolbar.js
@@ -1,173 +1,197 @@
 'use strict';
 
+
 var React = require('react'),
-	ReactDOMServer = require('react-dom/server'),
-	T = React.PropTypes;
+   ReactDOMServer = require('react-dom/server'),
+   T = require('prop-types'),
+   createClass = require('create-react-class'),
+   DOM = require('react-dom-factories');
+
 
 var defaultColors = [
-	'rgb(  0,   0,   0)', 'rgb(230,   0,   0)', 'rgb(255, 153,   0)',
-	'rgb(255, 255,   0)', 'rgb(  0, 138,   0)', 'rgb(  0, 102, 204)',
-	'rgb(153,  51, 255)', 'rgb(255, 255, 255)', 'rgb(250, 204, 204)',
-	'rgb(255, 235, 204)', 'rgb(255, 255, 204)', 'rgb(204, 232, 204)',
-	'rgb(204, 224, 245)', 'rgb(235, 214, 255)', 'rgb(187, 187, 187)',
-	'rgb(240, 102, 102)', 'rgb(255, 194, 102)', 'rgb(255, 255, 102)',
-	'rgb(102, 185, 102)', 'rgb(102, 163, 224)', 'rgb(194, 133, 255)',
-	'rgb(136, 136, 136)', 'rgb(161,   0,   0)', 'rgb(178, 107,   0)',
-	'rgb(178, 178,   0)', 'rgb(  0,  97,   0)', 'rgb(  0,  71, 178)',
-	'rgb(107,  36, 178)', 'rgb( 68,  68,  68)', 'rgb( 92,   0,   0)',
-	'rgb(102,  61,   0)', 'rgb(102, 102,   0)', 'rgb(  0,  55,   0)',
-	'rgb(  0,  41, 102)', 'rgb( 61,  20,  10)',
+   'rgb(  0,   0,   0)', 'rgb(230,   0,   0)', 'rgb(255, 153,   0)',
+   'rgb(255, 255,   0)', 'rgb(  0, 138,   0)', 'rgb(  0, 102, 204)',
+   'rgb(153,  51, 255)', 'rgb(255, 255, 255)', 'rgb(250, 204, 204)',
+   'rgb(255, 235, 204)', 'rgb(255, 255, 204)', 'rgb(204, 232, 204)',
+   'rgb(204, 224, 245)', 'rgb(235, 214, 255)', 'rgb(187, 187, 187)',
+   'rgb(240, 102, 102)', 'rgb(255, 194, 102)', 'rgb(255, 255, 102)',
+   'rgb(102, 185, 102)', 'rgb(102, 163, 224)', 'rgb(194, 133, 255)',
+   'rgb(136, 136, 136)', 'rgb(161,   0,   0)', 'rgb(178, 107,   0)',
+   'rgb(178, 178,   0)', 'rgb(  0,  97,   0)', 'rgb(  0,  71, 178)',
+   'rgb(107,  36, 178)', 'rgb( 68,  68,  68)', 'rgb( 92,   0,   0)',
+   'rgb(102,  61,   0)', 'rgb(102, 102,   0)', 'rgb(  0,  55,   0)',
+   'rgb(  0,  41, 102)', 'rgb( 61,  20,  10)',
 ].map(function(color){ return { value: color } });
+
 
 var defaultItems = [
 
-	{ label:'Formats', type:'group', items: [
-		{ label:'Font', type:'font', items: [
-			{ label:'Sans Serif',  value:'sans-serif', selected:true },
-			{ label:'Serif',       value:'serif' },
-			{ label:'Monospace',   value:'monospace' }
-		]},
-		{ label:'Size', type:'size', items: [
-			{ label:'Small',  value:'10px' },
-			{ label:'Normal', value:'13px', selected:true },
-			{ label:'Large',  value:'18px' },
-			{ label:'Huge',   value:'32px' }
-		]},
-		{ label:'Alignment', type:'align', items: [
-			{ label:'', value:'', selected:true },
-			{ label:'', value:'center' },
-			{ label:'', value:'right' },
-			{ label:'', value:'justify' }
-		]}
-	]},
 
-	{ label:'Text', type:'group', items: [
-		{ type:'bold', label:'Bold' },
-		{ type:'italic', label:'Italic' },
-		{ type:'strike', label:'Strike' },
-		{ type:'underline', label:'Underline' },
-		{ type:'color', label:'Color', items:defaultColors },
-		{ type:'background', label:'Background color', items:defaultColors },
-		{ type:'link', label:'Link' }
-	]},
+   { label:'Formats', type:'group', items: [
+       { label:'Font', type:'font', items: [
+           { label:'Sans Serif',  value:'sans-serif', selected:true },
+           { label:'Serif',       value:'serif' },
+           { label:'Monospace',   value:'monospace' }
+       ]},
+       { label:'Size', type:'size', items: [
+           { label:'Small',  value:'10px' },
+           { label:'Normal', value:'13px', selected:true },
+           { label:'Large',  value:'18px' },
+           { label:'Huge',   value:'32px' }
+       ]},
+       { label:'Alignment', type:'align', items: [
+           { label:'', value:'', selected:true },
+           { label:'', value:'center' },
+           { label:'', value:'right' },
+           { label:'', value:'justify' }
+       ]}
+   ]},
 
-	{ label:'Blocks', type:'group', items: [
-		{ type:'list', value:'bullet' },
-		{ type:'list', value:'ordered' }
-	]},
 
-	{ label:'Blocks', type:'group', items: [
-		{ type:'image', label:'Image' }
-	]}
+   { label:'Text', type:'group', items: [
+       { type:'bold', label:'Bold' },
+       { type:'italic', label:'Italic' },
+       { type:'strike', label:'Strike' },
+       { type:'underline', label:'Underline' },
+       { type:'color', label:'Color', items:defaultColors },
+       { type:'background', label:'Background color', items:defaultColors },
+       { type:'link', label:'Link' }
+   ]},
+
+
+   { label:'Blocks', type:'group', items: [
+       { type:'list', value:'bullet' },
+       { type:'list', value:'ordered' }
+   ]},
+
+
+   { label:'Blocks', type:'group', items: [
+       { type:'image', label:'Image' }
+   ]}
+
 
 ];
 
-var QuillToolbar = React.createClass({
 
-	displayName: 'Quill Toolbar',
+var QuillToolbar = createClass({
 
-	propTypes: {
-		id:        T.string,
-		className: T.string,
-		items:     T.array
-	},
 
-	getDefaultProps: function(){
-		return {
-			items: defaultItems
-		};
-	},
+   displayName: 'Quill Toolbar',
 
-	renderGroup: function(item, key) {
-		return React.DOM.span({
-			key: item.label || key,
-			className:'ql-formats' },
-			item.items.map(this.renderItem)
-		);
-	},
 
-	renderChoiceItem: function(item, key) {
-		return React.DOM.option({
-			key: item.label || item.value || key,
-			value:item.value },
-			item.label
-		);
-	},
+   propTypes: {
+       id:        T.string,
+       className: T.string,
+       items:     T.array
+   },
 
-	renderChoices: function(item, key) {
-		var attrs = {
-			key: item.label || key,
-			title: item.label,
-			className: 'ql-'+item.type
-		};
-		var self = this;
-		var choiceItems = item.items.map(function(item, key) {
-			if (item.selected) {
-				attrs.defaultValue = item.value;
-			}
-			return self.renderChoiceItem(item, key);
-		})
-		return React.DOM.select(attrs, choiceItems);
-	},
 
-	renderButton: function(item, key) {
-		return React.DOM.button({
-			type: 'button',
-			key: item.label || item.value || key,
-			value: item.value,
-			className: 'ql-'+item.type,
-			title: item.label },
-			item.children
-		);
-	},
+   getDefaultProps: function(){
+       return {
+           items: defaultItems
+       };
+   },
 
-	renderAction: function(item, key) {
-		return React.DOM.span({
-			key: item.label || item.value || key,
-			className: 'ql-'+item.type,
-			title: item.label },
-			item.children
-		);
-	},
 
-	renderItem: function(item, key) {
-		switch (item.type) {
-			case 'group':
-				return this.renderGroup(item, key);
-			case 'font':
-			case 'align':
-			case 'size':
-			case 'color':
-			case 'background':
-				return this.renderChoices(item, key);
-			case 'bold':
-			case 'italic':
-			case 'underline':
-			case 'strike':
-			case 'link':
-			case 'list':
-			case 'image':
-				return this.renderButton(item, key);
-			default:
-				return this.renderAction(item, key);
-		}
-	},
+   renderGroup: function(item, key) {
+       return DOM.span({
+           key: item.label || key,
+           className:'ql-formats' },
+           item.items.map(this.renderItem)
+       );
+   },
 
-	getClassName: function() {
-		return 'quill-toolbar ' + (this.props.className||'');
-	},
 
-	render: function() {
-		var children = this.props.items.map(this.renderItem);
-		var html = children.map(ReactDOMServer.renderToStaticMarkup).join('');
-		return React.DOM.div({
-			className: this.getClassName(),
-			style: this.props.style || {},
-			dangerouslySetInnerHTML: { __html:html }
-		});
-	}
+   renderChoiceItem: function(item, key) {
+       return DOM.option({
+           key: item.label || item.value || key,
+           value:item.value },
+           item.label
+       );
+   },
+
+
+   renderChoices: function(item, key) {
+       var attrs = {
+           key: item.label || key,
+           title: item.label,
+           className: 'ql-'+item.type
+       };
+       var self = this;
+       var choiceItems = item.items.map(function(item, key) {
+           if (item.selected) {
+               attrs.defaultValue = item.value;
+           }
+           return self.renderChoiceItem(item, key);
+       })
+       return DOM.select(attrs, choiceItems);
+   },
+
+
+   renderButton: function(item, key) {
+       return DOM.button({
+           type: 'button',
+           key: item.label || item.value || key,
+           value: item.value,
+           className: 'ql-'+item.type,
+           title: item.label },
+           item.children
+       );
+   },
+
+
+   renderAction: function(item, key) {
+       return DOM.span({
+           key: item.label || item.value || key,
+           className: 'ql-'+item.type,
+           title: item.label },
+           item.children
+       );
+   },
+
+
+   renderItem: function(item, key) {
+       switch (item.type) {
+           case 'group':
+               return this.renderGroup(item, key);
+           case 'font':
+           case 'align':
+           case 'size':
+           case 'color':
+           case 'background':
+               return this.renderChoices(item, key);
+           case 'bold':
+           case 'italic':
+           case 'underline':
+           case 'strike':
+           case 'link':
+           case 'list':
+           case 'image':
+               return this.renderButton(item, key);
+           default:
+               return this.renderAction(item, key);
+       }
+   },
+
+
+   getClassName: function() {
+       return 'quill-toolbar ' + (this.props.className||'');
+   },
+
+
+   render: function() {
+       var children = this.props.items.map(this.renderItem);
+       var html = children.map(ReactDOMServer.renderToStaticMarkup).join('');
+       return DOM.div({
+           className: this.getClassName(),
+           style: this.props.style || {},
+           dangerouslySetInnerHTML: { __html:html }
+       });
+   }
+
 
 });
+
 
 module.exports = QuillToolbar;
 QuillToolbar.defaultItems = defaultItems;
